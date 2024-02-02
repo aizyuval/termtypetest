cf5f305fdb13f8bdd0c7cfda4ce32da3bbc37f4f
aizyuval <yuvalaizenman@gmail.com>
2024-02-02 04:32:36 -0500

    added semi-working linked list structural test and result printing

--- typtest.c
+++ typtest.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
@@ -8,9 +9,8 @@
 struct _word {
 	char * string;
 	char length;
-	char correct;
-	char mistake;
-	char overword;
+	char * ver_arr; // 0=wrong,1=true,2=none
+	char overword; // mistakes are here once relCursor is over length (instead of in ver_arr) IMPORTANT
 	struct _word * prev_word;
 	struct _word * next_word;
 };
@@ -32,43 +32,39 @@ int main()
 	char * strings[] = {"word", "school", "thought", "man", "mongol", "horse", "paper", "china", "bottle", "child", "directly", "start", "mode", "setting", "headphone"}; //TEMP
 	size_t wordAmount = sizeof(strings)/sizeof(const char *);// temp, will be in data file
 	size_t _word_size = sizeof(struct _word);
-	struct _word *linked_list1, *linked_list2, *linked_list3;
-	linked_list1 = malloc(_word_size);
-	linked_list2 = linked_list1;
-	linked_list3 = linked_list1;
+	// 3 linked lists that point to the same place, but are being used in different place, for convenience. initializer_ll is for initializing values in the ll. dynamic_ll is for changing these values as the user types. printed_ll is for caluclating the stats over the ll
+	struct _word *initializer_ll, *dynamic_ll, *printed_ll;
+	initializer_ll = malloc(_word_size);
+	dynamic_ll = initializer_ll;
+	printed_ll = initializer_ll;
 
-	for(size_t i=0; i<wordAmount; linked_list1=linked_list1->next_word){
+	for(size_t i=0; i<wordAmount; initializer_ll=initializer_ll->next_word){
 		// initialization of linked list
 
-		linked_list1->string = malloc(strlen(strings[i]));
-		strcpy(linked_list1->string, strings[i]);
-		linked_list1->length = (char)strlen(linked_list1->string);
-		linked_list1->correct = 0;
-		linked_list1->mistake = 0;
-		linked_list1->overword = 0;
-		linked_list1->prev_word = malloc(_word_size);
-		linked_list1->next_word = malloc(_word_size);
+		initializer_ll->string = malloc(strlen(strings[i]));
+		strcpy(initializer_ll->string, strings[i]);
+		printw("%s ", initializer_ll->string);
+		initializer_ll->ver_arr = malloc(strlen(strings[i]));
+		memset(initializer_ll->ver_arr, 2, strlen(strings[i]));
+		
+		initializer_ll->length = (char)strlen(initializer_ll->string);
+		initializer_ll->overword = 0;
+		initializer_ll->prev_word = malloc(_word_size);
+		initializer_ll->next_word = malloc(_word_size);
 		i++;
-		// later when enumerating for print, i can simply do while string!=NULL. it will be in the last linked element, that has allocated space but has no values.
+		// later when enumerating for print, i can simply do while dynamic_ll->string!=NULL. it will be in the last linked element, that has allocated space but has no values.
 	}
-	// when printing a string. printw("%s "); -> space is very important
+
+	// when printing a dynamic_ll->string. printw("%s "); -> space is very important
 // print strings
 	//
+	//new variables: char relCurser, spaces, 
 	// start while loop, 
-	// compare recvChar with linked_list->string[relCursor]
+	// compare recvChar with ->dynamic_ll->string[relCursor]
 	//
-	// relCurser is used for comparison of the current char[relCurser] with recvChar. when he backspaces, it becomes relCurser --; when he backspaces a space, it becomes l->length (ast elemnt of string); when he spaces, it becomes 0;
+	// relCurser is used for comparison of the current char[relCurser] with recvChar. when he backspaces, it becomes relCurser --; when he backspaces a space, it becomes l->length (ast elemnt of dynamic_ll->string); when he spaces, it becomes 0;
 	// relCurser is also a representation of the completed chars. it ++'s with ever addch.
 	//
-	// when he spaces:
-	// if next_word->string is null, he is a god and finished everthing(what about mistakes?)
-	//
-	// if(relcurser<l->length ) -> space in the middle of the word
-	// {move to (getxy) + length-relcurses (it's not length-1, because relcurses is on the key that he didn't type yet. thus, num of complete keys is actually relcurses
-	// 
-	// else -> space before space (good); {}
-	// addch, space++, linked_list = linked_list->next_word. relCurser = 0; 
-	//(CHECK - what is more costly, if'ing and sometimes moving, or moving all the time (to length-relcursor right?)
 	//
 	//when he backspaces:
 	//if relcursor=0:
@@ -89,40 +85,25 @@ int main()
 	//
 	//WHAT IF HE BACKSPACES THE FIRST CHAR?
 	//
-	//when he types a regular char:
-	//if(relCursor >= l->length) -> he types when he should have space'd (overword, or simply space)
-	// l->overword++;
-	// l->mistakes++;
-	//add the char, push all the text right or down, relCursor++
-	//else // types where he should
-		// if recvChar = l->string[relCurser]
-//		//  addch(recvChar | green)
-//		//  l->correct++;
-//		// else 
-//              // addch(recvChar | red)
-//              // l->mistake++;
-//              // 
-//              // relcurser++ -> both;
-
 	//
 	// current global curser is: get(stdscr, row, col)
 	// current global curser is: enumeration over linked_list lengths plus spaces
 
 	// if l->correct != length then word is incorrect
 	// else its all correct and word is complete
-	// 
+	//  make sure everything is continued
 
-	printw("%s\n", string); //TEMP - should print dynamically?
 
-	int count = 0, mistakes = 0, middleRun = 0, spaces = 0;
-	move(0, 0); // move the cursor to where the first char of the string is printed //TEMP
+	int middleRun = 0, spaces = 0, row = 0, col = 0;
+	char relCursor = 0;
+	move(0, 0); // move the cursor to where the first char of the dynamic_ll->string is printed //TEMP
 	const long time_out = 15;
-
+	// dynamic_ll->string[relCursor] becomes dynamic_ll->string[relcurser]
 	int recvChar = getch(); // getting the first keystroke, and timing from there.
 	long start =time(NULL);	
 	while ((time(NULL)-start) < time_out) { 
 		timeout(1000*(time_out - (time(NULL)-start)));	
-		//if backspace, count --; else, check if similar
+		//if backspace, relCursor --; else, check if similar
 		if(middleRun){ 
 			recvChar = getch();
 			if(recvChar == ERR){
@@ -133,57 +114,119 @@ int main()
 
 		}
 
-		// change the word/space in location count. This can be applied when I know how to represent char as a variable on the screen
+		// change the word/space in location relCursor. This can be applied when I know how to represent char as a variable on the screen
 		if (recvChar == KEY_BACKSPACE){
 			// what if he backspaces a ' ' -> space?
-			count--;// -1 location
-			// if the key was correct, delete one correct. if it was mistake, delete mistake
-			mvaddch(0, count, string[count]);
-			move(0, count);
+	//when he backspaces:
+	//if relcursor=0:
+	//space--, linked_list = linked_list->prev_word;
+	//relcursor= l->overword + l->length
+	// mvaddch, move one back 
+	// 
+	//if relcursor > l->length
+	// he backspaces an overworded
+	// delch
+	//  l->overword--;
+	//  relCursor--;
+	// 
+	// else // the curser is inside the word
+	// relCursor--;
+	// mvaddch 
+			// ALSO: if backspace and the word is not finished, go to where it didn't finish
+			// moveone abck
+			getyx(stdscr, row, col);
+			if(relCursor==0){
+				// he backspaces a space
+				move(row,col-1);	
+				spaces--;
+				dynamic_ll = dynamic_ll->prev_word;
+				relCursor = dynamic_ll->overword + dynamic_ll->length;
+			}else if(relCursor<=dynamic_ll->length){
+				// he is in the word
+				relCursor--;	
+				dynamic_ll->ver_arr[relCursor] = 2;// correct or not, it is deleted and gets 2
+				mvaddch(row, col-1,dynamic_ll->string[relCursor] );
+				move(row, col-1);
+			}else{
+				//backspace overword
+				dynamic_ll->overword -= 1;
+				delch(); // CHECK
+				move(row, col-1);
+			}
+				
+		}
+		else if(recvChar == ' '){
+	// if next_word->dynamic_ll->string is null, he is a god and finished everthing(what about mistakes?)
+			if (relCursor<dynamic_ll->length){
+				//he spaces in the middle of the word
+				//tab to the next word, and add the rest of chars of the word as mistakes
+				memset(dynamic_ll->ver_arr, 0, (dynamic_ll->length - relCursor)); // consider the rest of the chars as wrong
+				getyx(stdscr, row, col);
+				move(row, col+(int)(dynamic_ll->length - relCursor));// what if it drops a line? ERROR
+			}
+			addch(recvChar);
+			dynamic_ll=dynamic_ll->next_word;
+			spaces++;
+			relCursor=0;
 			continue;
 		}
-		else if( recvChar == ' ' || string[count] == ' ') { // if he key'd space, or should have;
-			if(recvChar == string[count] && recvChar == ' '){ 
-			// space regularly
-				printw("%c", recvChar); // no colors. it's just a space.	
-				spaces++;
-			}else if (recvChar == ' ' && string[count] != recvChar){
-				// implement when underlying structure is different
-				//spaces++;
-				// tab to the next word, location can be +3 or +2 or +6. add to mistakes the number of chars that is left.
-				continue;// temp
-			}else {
-				// he didn't space when he should have. add to mistakes.
-				// print the word and don't advance in location(continue). wait for him to type space.
-				// implement when backspace is available
-				continue;//temp
-			}
-		}else  {
-			if(recvChar == string[count]){ // change the underlying process for determining location
-				addch(recvChar | COLOR_PAIR(3));
-			}else {
-				addch(string[count] | COLOR_PAIR(2));// adds the same char, only red. //FIX - IS THERE A BETTER WAY, then to add the same char?
-				mistakes++;
-
-			}
+		else if (relCursor >= dynamic_ll->length){
+			// recvChar isn't space although it should be.
+			dynamic_ll->overword += 1; // will add overword to later mistakes
+			relCursor++;
+			insch(recvChar); //add the char and push all to the right
+			//move(row, jj)
+			//add the char, push all the text right or down, ERROR CURRENTLY
+			continue;
+			
 		}
-		count++;// move to next location (char)
+		else  { // normal key is needed, and normal key is given (cause he's not upon space, relCursor<d->length)
+			if(recvChar == dynamic_ll->string[relCursor]){ // change the underlying process for determining location
+				addch(recvChar | COLOR_PAIR(3));//green
+				dynamic_ll->ver_arr[relCursor] = 1;
+			}else {
+				addch(dynamic_ll->string[relCursor] | COLOR_PAIR(2));// adds the same char, only red. //FIX - IS THERE A BETTER WAY, then to add the same char?
+				dynamic_ll->ver_arr[relCursor] = 0;
+			}
+			relCursor++;
+			
+		}
 	}
 	attroff(COLOR_PAIR(3));
 	attroff(COLOR_PAIR(2));
 	timeout(-1);//cancelling the timeout!!
 	start = time(NULL)-start;
 	printw("\nstart after elapsation: %lu \n", start); 
-	if (mistakes) { //1 mistake or more
-		printw("\n you have  %f accuracy, and raw wpm of: %d ", 100*((((float)count)-((float)mistakes))/((float)count)), ((count-mistakes)/5)*(60/(int)time_out)); 
-	}else{
-		printw("\n you have 100%% accuracy, and raw wpm of: %d", ((count-mistakes)/5)*(60/(int)time_out)); 
-	}
-	printw("\nYou had, %d mistakes", mistakes);	
 
+	float mistakes = 0, corrects = 0;
+	float charAmount = 0;
+	bool correctWord;
+	float correctWordChars = 0;
+	for( ;printed_ll!=dynamic_ll; printed_ll=printed_ll->next_word){
+		correctWord = true;	
+		for (int i =0; i<printed_ll->length; i++) {
+			if (printed_ll->ver_arr[i] ==1){
+				corrects++;
+			}else if (printed_ll->ver_arr[i] ==0){
+				mistakes++;
+				correctWord = false;
+			}
+			charAmount++;
+		}
+		if(correctWord){
+			correctWordChars+= printed_ll->length;
+		}
+
+	}
+	// calculations: raw wpm, wpm, accuracy.
+	// raw wpm:	all chars/5 * 60/time_out
+	// wpm: all chars in correct words/5 * 60/time_out
+	// accuracy: correct chars/all chars * 100
+	// attention to floats or so
+	printw("wpm: %f, raw wpm: %f, accuracy: %f", (correctWordChars/5.0)*(60.0/((float)time_out)), (charAmount/5)*(60/((float)time_out)), (corrects/charAmount)*100);
 	refresh(); 
 	getch(); // only leave the system if  he c-c, or other known key
-	endwin();
+endwin();
 
 	return 0;
 }
